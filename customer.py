29f064fc131454bceb101ca2f9cee28c35dcbcf7
some random idiot <sittipat2554aa@hotmail.com>
2021-10-11 20:58:35 +0700

    Removed the use of temp variable in the statement function.

--- customer.py
+++ customer.py
@@ -37,16 +37,15 @@ class Customer:
         fmt = "{:32s}   {:4d} {:6.2f}\n"
 
         for rental in self.rentals:
-            amount = rental.get_charge(rental)
             # award renter points
             if rental.get_movie().get_price_code() == Movie.NEW_RELEASE:
                 frequent_renter_points += rental.get_days_rented()
             else:
                 frequent_renter_points += 1
             #  add detail line to statement
-            statement += fmt.format(rental.get_movie().get_title(), rental.get_days_rented(), amount)
+            statement += fmt.format(rental.get_movie().get_title(), rental.get_days_rented(), rental.get_charge(rental))
             # and accumulate activity
-            total_amount += amount
+            total_amount += rental.get_charge(rental)
 
         # footer: summary of charges
         statement += "\n"
